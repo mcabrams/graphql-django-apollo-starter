8466be625df8613f399cc0b27989b4228cc35f88
Matthew Abrams <mcabrams1@gmail.com>
2020-03-21 18:36:56 -0700

    Prevent resize observer loop error from failing cypress tests

--- {{ cookiecutter.project_slug }}/e2e/cypress/support/index.js
+++ {{ cookiecutter.project_slug }}/e2e/cypress/support/index.js
@@ -1,4 +1,12 @@
 import './commands';
+const resizeObserverLoopErrRe = /^ResizeObserver loop limit exceeded/
+
+Cypress.on('uncaught:exception', (err) => {
+  if (resizeObserverLoopErrRe.test(err.message)) {
+    // returning false here prevents Cypress from failing the test
+    return false;
+  }
+});
 
 beforeEach(() => {
   cy.visit('/');
